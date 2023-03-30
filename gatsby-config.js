45d25b33949729700cadf809e946570c5357328a
hyunwlee-dev <hwlee3928@gmail.com>
2023-03-31 00:53:27 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `hyunwlee-blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
